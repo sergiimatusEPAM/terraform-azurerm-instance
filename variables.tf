5eb67260e6e56fa0899b02bbbc1cdf94148a1084
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:55:59 +0200

    Automatic update of variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,80 +1,112 @@
 # Number of Instance
-variable "num_instances" {}
+variable "num_instances" {
+  description = "num instances"
+}
 
 # Location (region)
-variable "location" {}
+variable "location" {
+  description = "location"
+}
 
 # Cluster Name
-variable "name_prefix" {}
+variable "name_prefix" {
+  description = "Cluster Name"
+}
 
 # Instance Type
-variable "instance_type" {}
+variable "instance_type" {
+  description = "instance type"
+}
 
 # DCOS Version for prereq install
-variable "dcos_version" {}
+variable "dcos_version" {
+  description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
+}
 
 # Tested OSes to install with prereq
-variable "dcos_instance_os" {}
+variable "dcos_instance_os" {
+  description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
+}
 
 # Private SSH Key Filename Optional
 variable "ssh_private_key_filename" {
+  description = "Path to the SSH private key"
   # cannot leave this empty as the file() interpolation will fail later on for the private_key local variable
   # https://github.com/hashicorp/terraform/issues/15605
   default = "/dev/null"
 }
 
 # Source image to boot from. We assume the user has already take care of the prereq during this step.
-variable "image" {}
+variable "image" {
+  description = "image"
+}
 
 # Disk Type to Leverage. The managed disk type. (optional)
 variable "disk_type" {
+  description = "Disk Type to Leverage."
   default = "Standard_LRS"
 }
 
 # Disk Size in GB
-variable "disk_size" {}
+variable "disk_size" {
+  description = "disk size"
+}
 
 # Resource Group Name
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  description = "resource group name"
+}
 
 # Customer Provided Userdata
 variable "user_data" {
+  description = "User data to be used on these instances (cloud-init)"
   default = ""
 }
 
 # SSH User
-variable "admin_username" {}
+variable "admin_username" {
+  description = "admin username"
+}
 
 # SSH Public Key
-variable "public_ssh_key" {}
+variable "public_ssh_key" {
+  description = "public ssh key"
+}
 
 # Add special tags to the resources created by this module
 variable "tags" {
+  description = "Add custom tags to all resources"
   type    = "map"
   default = {}
 }
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
+  description = "Format the hostname inputs are index+1, region, cluster_name"
   default = "instance-%[1]d-%[2]s"
 }
 
 # Public backend address pool 
 variable "public_backend_address_pool" {
+  description = "public backend address pool"
   type    = "list"
   default = []
 }
 
 # Private backend address pool 
 variable "private_backend_address_pool" {
+  description = "private backend address pool"
   type    = "list"
   default = []
 }
 
 # Security Group Id
 variable "network_security_group_id" {
+  description = "network security group id"
   default = ""
 }
 
 # Subnet ID
-variable "subnet_id" {}
+variable "subnet_id" {
+  description = "Subnet ID"
+}
