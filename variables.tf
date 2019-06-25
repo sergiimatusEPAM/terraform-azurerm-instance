471379f360aae15e71bcb3b5bb0b0c973efe0764
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-25 11:34:49 +0200

    chore: updated variable descriptions with tfdescsan

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 }
 
 variable "cluster_name" {
-  description = "Cluster Name"
+  description = "Name of the DC/OS cluster"
 }
 
 variable "vm_size" {
@@ -15,7 +15,7 @@ variable "vm_size" {
 }
 
 variable "dcos_version" {
-  description = "Specifies which DC/OS version instruction to use. Options: 1.12.3, 1.11.10, etc. See dcos_download_path or dcos_version tree for a full list."
+  description = "Specifies which DC/OS version instruction to use. Options: 1.13.1, 1.12.3, 1.11.10, etc. See dcos_download_path or dcos_version tree for a full list."
 }
 
 variable "dcos_instance_os" {
