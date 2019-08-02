8f5ce2d7b04df3286b7417b988bd4fc1b1ebc449
Sergii Matus <sergii_matus@epam.com>
2019-08-02 12:04:39 +0300

    Splitted windows-instance module from instance module

--- variables.tf
+++ variables.tf
@@ -105,8 +105,3 @@ variable "avset_platform_fault_domain_count" {
   description = "Availability set platform fault domain count, differs from location to location"
   default     = 3
 }
-
-variable "is_windows" {
-  description = "Flag to distinquish instance from Windows, by default Linux takes priority"
-  default     = 0
-}
